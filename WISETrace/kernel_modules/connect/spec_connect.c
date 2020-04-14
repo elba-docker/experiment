f588c05e124c75a7f63b8d15e008adfdadac40de
Joseph Azevedo <joseph.az@gatech.edu>
2020-04-14 09:28:02 -0400

    test: change names

--- WISETrace/kernel_modules/connect/spec_connect.c
+++ WISETrace/kernel_modules/connect/spec_connect.c
@@ -102,9 +102,9 @@ static int __init specialize_connect(void) {
       &connect_proc_ops);
   // Prevent gcc warnings on writing to readonly array by invoking xchg
   sys_call_ptr_t* sys_call_table_hack = sys_call_table;
-  sys_call_ptr_t** sys_call_table_hack = sys_call_table;
-  sys_call_ptr_t* sys_call_table_hack = &sys_call_table;
-  sys_call_ptr_t** sys_call_table_hack = &sys_call_table;
+  sys_call_ptr_t** sys_call_table_hack_1 = sys_call_table;
+  sys_call_ptr_t* sys_call_table_hack_2 = &sys_call_table;
+  sys_call_ptr_t** sys_call_table_hack_3 = &sys_call_table;
   original_connect = (void *) xchg(&(*sys_call_table_hack)[__NR_connect], specialized_connect);
 
   printk(KERN_INFO "Specialized connect syscall.\n");
