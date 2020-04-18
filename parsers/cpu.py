aa847f38ea5d18422332bfdbb2a48f2779337382
Joseph Azevedo <joseph.az@gatech.edu>
2020-04-17 21:40:01 -0400

    feat: clean up repo

--- parsers/cpu.py
+++ parsers/cpu.py
@@ -1,12 +1,13 @@
 import datetime
 import sys
+from collections import OrderedDict
 
 
 class CpuEntry:
     """A CPU entry."""
 
     def __init__(self, user, nice, system, wait, irq, soft, steal, idle, total, guest, guest_n,
-            intrpt):
+            intrpt, timestamp):
         """Initialize a CpuEntry."""
         self._user = user
         self._nice = nice
@@ -20,6 +21,7 @@ class CpuEntry:
         self._guest = guest
         self._guest_n = guest_n
         self._intrpt = intrpt
+        self._timestamp = timestamp
 
     def user(self):
         return self._user
@@ -56,35 +58,33 @@ class CpuEntry:
 
     def intrpt(self):
         return self._intrpt
+    
+    def time(self):
+        return self._timestamp
 
 
-def main():
+def main(iterator):
     # List of timestamps and CpuEntry for each CPU.
-    timestamps = []
+    # timestamps = []
     cpu_entries = []
     # Process CPU raw file.
-    with open(sys.argv[1]) as cpu_file:
-        for cpu_line in cpu_file:
-            # Check if it is a comment.
-            if cpu_line[0] == '#':
-                continue
-            cpu_entry_data = cpu_line.split()
-            timestamps.append(datetime.datetime.strptime(cpu_entry_data[1], "%H:%M:%S.%f"))
-            for cpu_no in range((len(cpu_entry_data) - 2) // 12):
-                if len(cpu_entries) < cpu_no + 1:
-                    cpu_entries.append([])
-                cpu_entries[cpu_no].append(
-                        CpuEntry(*cpu_entry_data[cpu_no * 12 + 2:cpu_no * 12 + 14])
-                )
-    # Write total utilization of each CPU.
-    for cpu_no in range(len(cpu_entries)):
-        if sum([int(cpu_entry.total()) for cpu_entry in cpu_entries[cpu_no]]) > 0:
-            with open("cpu%s.data" % cpu_no, 'w') as cpu_util_file:
-                for (timestamp, cpu_entry) in zip(timestamps, cpu_entries[cpu_no]):
-                    td = timestamp - timestamps[0]
-                    cpu_util_file.write("%s.%s %s\n" % (
-                        td.seconds, td.microseconds, cpu_entry.total()
-                    ))
+    for cpu_line in iterator:
+        # Check if it is a comment.
+        if cpu_line[0] == '#':
+            continue
+        cpu_entry_data = cpu_line.split()
+        timestamp = datetime.datetime.strptime(cpu_entry_data[1], "%H:%M:%S.%f")
+        # timestamps.append(datetime.datetime.strptime(cpu_entry_data[1], "%H:%M:%S.%f"))
+        for cpu_no in range((len(cpu_entry_data) - 2) // 12):
+            if len(cpu_entries) < cpu_no + 1:
+                cpu_entries.append(OrderedDict())
+            cpu_entries[cpu_no][timestamp] = CpuEntry(*cpu_entry_data[cpu_no * 12 + 2:cpu_no * 12 + 14], timestamp)
 
-if __name__ == "__main__":
-    main()
+    return cpu_entries
+    # Write total utilization of each CPU.
+    # for cpu_no in range(len(cpu_entries)):
+    #     if sum([int(cpu_entry.total()) for cpu_entry in cpu_entries[cpu_no]]) > 0:
+    #         with open("cpu%s.data" % cpu_no, 'w') as cpu_util_file:
+    #             for (timestamp, cpu_entry) in zip(timestamps, cpu_entries[cpu_no]):
+    #                 td = timestamp - timestamps[0]
+    #                 cpu_util_file.write("%s.%s %s\n" % (td.seconds, td.microseconds, cpu_entry.total()))
--- vis/requests_per_sec.gnuplot
+++
@@ -1,9 +0,0 @@
-set terminal png size 900,400
-set ylabel "# Requests"
-set xlabel "Time (seconds)"
-set xdata time
-set timefmt "%s"
-set format x "%s"
-set key left top
-set grid
-plot "requests_per_sec.data" using 1:2 with lines title ""
